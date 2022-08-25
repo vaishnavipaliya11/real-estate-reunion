963b6b527dc1234d8fe9f10581b35b1aa3ebb3ce
Vaishnavi Paliya <vaishnavipaliya11@gmail.com>
2022-08-25 13:22:31 +0530

    chore - react router and home page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import { Home } from './Pages/Home';
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <h2>Real Estate</h2>
+      <Routes>
+        <Route path="/" element = {<Home/>}/>
+      </Routes>
     </div>
   );
 }
